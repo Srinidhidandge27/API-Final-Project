912d0af0127e3c724560a35ff0f9999d1ed4d2b9
Srinidhi Dandge <nidhidandge@gmail.com>
2025-02-17 10:19:30 -0600

    delete

--- backend/models/Player.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const PlayerSchema = new mongoose.Schema({
-   name: String,
-   position: String,
-   team: String,
-});
-
-export default mongoose.model('Player', PlayerSchema);
